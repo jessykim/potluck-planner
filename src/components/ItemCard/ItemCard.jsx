fea640674826354a2244e768801aa0bf3c2fbdbe
jessikim <jessica.kim17@gmail.com>
2022-12-21 16:41:07 -0600

    style itemcard

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom"
+import styles from './ItemCard.module.css'
 
 const ItemCard = ({ item, user, potluckId, handleDeleteItem }) => {
   return (
-    <article>
+    <div className={styles.itemCard}>
       <p>{user.name}</p>
-      <p>{item.name}</p>
-      <p>{item.category}</p>
-      <p>{item.notes}</p>
-      <span>
-        {item.provider === user.profile &&
-          <>
-            <Link 
-              to={`/potlucks/${potluckId}/items/${item._id}`} 
-              state={item}
-            >
-              Edit
-            </Link>
-            <button onClick={() => handleDeleteItem(potluckId, item._id)}>Delete</button>
-          </>
-        }
-      </span>
-    </article>
+      <p>Bringing: {item.name}</p>
+      <p>Category: {item.category}</p>
+      {item.notes ? <p>Note: {item.notes}</p> : ""}
+      {item.provider === user.profile &&
+        <div className={styles.btns}>
+          <Link 
+            to={`/potlucks/${potluckId}/items/${item._id}`} 
+            state={item}
+          >
+            <button>Edit</button>
+          </Link>
+          <button onClick={() => handleDeleteItem(potluckId, item._id)}>Delete</button>
+        </div>
+      }
+    </div>
   )
 }
 
