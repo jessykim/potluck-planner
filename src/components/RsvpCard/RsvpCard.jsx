3a89af6d49228225b3ca394779021c42b2792e86
jessikim <jessica.kim17@gmail.com>
2022-12-21 14:30:07 -0600

    style rsvp form and card

--- src/components/RsvpCard/RsvpCard.jsx
+++ src/components/RsvpCard/RsvpCard.jsx
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom"
+import styles from './RsvpCard.module.css'
 
 const RsvpCard = ({ rsvp, user, potluckId, handleDeleteRsvp }) => {
   return (
-    <article>
-      <p>{user.name}</p>
-      <p>{rsvp.party}</p>
-      <p>{rsvp.notes}</p>
-      <p>{rsvp.rsvp}</p>
-      <span>
-        {rsvp.guest === user.profile &&
-          <>
-            <Link 
-              to={`/potlucks/${potluckId}/rsvps/${rsvp._id}`} 
-              state={rsvp}
-            >
-              Edit
-            </Link>
-            <button onClick={() => handleDeleteRsvp(potluckId, rsvp._id)}>Delete</button>
-          </>
-        }
-      </span>
-    </article>
+    <div className={styles.rsvpCard}>
+      <p>Guest: {user.name}</p>
+      <p>Party of {rsvp.party}</p>
+      {rsvp.notes ? <p>Comment: {rsvp.notes}</p> : ""}
+      <p>RSVP: {rsvp.rsvp}</p>
+      {rsvp.guest === user.profile &&
+        <div className={styles.btns}>
+          <Link 
+            to={`/potlucks/${potluckId}/rsvps/${rsvp._id}`} 
+            state={rsvp}
+          >
+            <button>Edit</button>
+          </Link>
+          <button onClick={() => handleDeleteRsvp(potluckId, rsvp._id)}>Delete</button>
+        </div>
+      }
+    </div>
   )
 }
 
