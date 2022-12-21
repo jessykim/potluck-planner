38bd17444aa528d1fcf24ab9e4be1a68e788db4b
jessikim <jessica.kim17@gmail.com>
2022-12-21 16:18:59 -0600

    style foodcard

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom"
+import styles from './FoodCard.module.css'
 
 const FoodCard = ({ food, user, potluckId, handleDeleteFood }) => {
   return (
-    <article>
+    <div className={styles.foodCard}>
       <p>{user.name}</p>
-      <p>{food.name}</p>
-      <p>{food.type}</p>
-      <p>{food.notes}</p>
-      <span>
-        {food.provider === user.profile &&
-          <>
-            <Link 
-              to={`/potlucks/${potluckId}/foods/${food._id}`} 
-              state={food}
-            >
-              Edit
-            </Link>
-            <button onClick={() => handleDeleteFood(potluckId, food._id)}>Delete</button>
-          </>
-        }
-      </span>
-    </article>
+      <p>Bringing: {food.name}</p>
+      <p>Category: {food.type}</p>
+      {food.notes ? <p>Note: {food.notes}</p> : ""}
+      {food.provider === user.profile &&
+        <div className={styles.btns}>
+          <Link 
+            to={`/potlucks/${potluckId}/foods/${food._id}`} 
+            state={food}
+          >
+            <button>Edit</button>
+          </Link>
+          <button onClick={() => handleDeleteFood(potluckId, food._id)}>Delete</button>
+        </div>
+      }
+    </div>
   )
 }
 
