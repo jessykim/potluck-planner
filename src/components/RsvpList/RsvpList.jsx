c3b482d1ff23fdf3a47c0b31b50d11524262ecaa
jessikim <jessica.kim17@gmail.com>
2022-12-21 20:24:03 -0600

    include message if no reservations are made

--- src/components/RsvpList/RsvpList.jsx
+++ src/components/RsvpList/RsvpList.jsx
@@ -1,8 +1,11 @@
 import RsvpCard from '../RsvpCard/RsvpCard'
 
 const RsvpList = (props) => {
-  if (!props.rsvps) return <h4>No Reservations</h4>
-
+  if (props.rsvps.length < 1) {
+    return (
+      <h1>No Reservations</h1>
+    )
+  }
   return (
     <>
       {props.rsvps.map((rsvp) => (
