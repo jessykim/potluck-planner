5e1e14b843bd2bf192856f39477ed99a0778dcae
jessikim <jessica.kim17@gmail.com>
2022-12-22 00:37:24 -0600

    style item form and edit form

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -25,54 +25,55 @@ const ItemForm = (props) => {
 
   return (
     <>
-      <form className={styles.container} onSubmit={handleSubmit}>
-        {/* <h1>{props.user.name}</h1> */}
-        <div className="form-group mb-3">
-          <label htmlFor="name-input" className="form-label">
-            Item
-          </label>
-          <input 
-            type="text" 
-            id="name-input"
-            name="name"
-            value={itemForm.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group mb-3">
-          <label htmlFor="category-input" className="form-label">
-            Category
-          </label>
-          <select 
-            name="category" 
-            id="category-input"
-            value={itemForm.category}
-            onChange={handleChange}
-            required
-          >
-            <option value="Bowls/Plates">Bowls/Plates</option>
-            <option value="Cups">Cups</option>
-            <option value="Decorations">Decorations</option>
-            <option value="Utensils">Utensils</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        <div className="form-group mb-3">
-          <label htmlFor="notes-input" className="form-label">
-            Additional Notes
-          </label>
-          <textarea
-            type="text"
-            name="notes"
-            id="notes-input"
-            value={itemForm.notes}
-            placeholder="Optional"
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-light btn-fluid">Submit</button>
-      </form>
+      <main className={styles.container}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className="form-group mb-3">
+            <label htmlFor="name-input" className="form-label">
+              Item
+            </label>
+            <input 
+              type="text" 
+              id="name-input"
+              name="name"
+              value={itemForm.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="category-input" className="form-label">
+              Category
+            </label>
+            <select 
+              name="category" 
+              id="category-input"
+              value={itemForm.category}
+              onChange={handleChange}
+              required
+            >
+              <option value="Bowls/Plates">Bowls/Plates</option>
+              <option value="Cups">Cups</option>
+              <option value="Decorations">Decorations</option>
+              <option value="Utensils">Utensils</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="notes-input" className="form-label">
+              Additional Notes
+            </label>
+            <textarea
+              type="text"
+              name="notes"
+              id="notes-input"
+              value={itemForm.notes}
+              placeholder="Optional"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-light btn-fluid">Submit</button>
+        </form>
+      </main>
     </>
   )
 }
