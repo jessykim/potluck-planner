47b05c6387fe644716166dc242c49f463e2614c0
jessikim <jessica.kim17@gmail.com>
2022-12-23 23:05:20 -0600

    change drinkcard to display provider name

--- src/components/DrinkCard/DrinkCard.jsx
+++ src/components/DrinkCard/DrinkCard.jsx
@@ -4,7 +4,7 @@ import styles from './DrinkCard.module.css'
 const DrinkCard = ({ drink, user, potluckId, handleDeleteDrink }) => {
   return (
     <div className={styles.drinkCard}>
-      <p>{user.name}</p>
+      <p>{drink.provider.name}</p>
       <p>Bringing: {drink.name}</p>
       {drink.alcoholic ?
         <p>ALCOHOLIC</p>
@@ -13,7 +13,7 @@ const DrinkCard = ({ drink, user, potluckId, handleDeleteDrink }) => {
       }
       {drink.quantity ? <p>Quantity: {drink.quantity}</p> : ""}
       {drink.notes ? <p>{drink.notes}</p> : ""}
-      {drink.provider === user.profile &&
+      {drink.provider._id === user.profile &&
         <div className={styles.btns}>
           <Link 
             to={`/potlucks/${potluckId}/drinks/${drink._id}`} 
